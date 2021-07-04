3cc8dc4a50aaeb65421fdfab0cffb07d20b6b8ec
Jarlish <elijahstathis@gmail.com>
2021-07-03 19:50:04 -0700

    Added the basics of the map

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,13 @@ struct Renderable {
     bg: RGB,
 }
 
-#[derive(Component)]
-struct LeftMover {}
-
 #[derive(Component, Debug)]
 struct Player {}
+
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall, Floor
+}
 
 //Create game state
 struct State {
@@ -40,8 +42,9 @@ impl GameState for State {
         //Run systems
         self.run_systems();
 
-        //Print hello world to the screen
-        ctx.print(1, 1, "Hello Rust World");
+        //Draw the map
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
 
         //Draw entities with a Position and a Renderable component to the screen
         let positions = self.ecs.read_storage::<Position>(); //Get read access to the ECS's Position component storage
@@ -53,14 +56,78 @@ impl GameState for State {
     }
 }
 
+//Function to get a unique index for each map tile from its position
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+//Constructor function for the map
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80*50];
+
+    //Make the boundaries of the map walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    //Randomly place 400 walls
+    //Obtain the thread-local RNG
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y); //Calculate the new walls location in the tile vector from its position
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+//Function to draw the map
+fn draw_map(map: &[TileType], ctx : &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+
+    for tile in map.iter() {
+        //Render the tile based on its type
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_u8(80, 55, 10), RGB::from_u8(0, 0, 0), rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_u8(160, 110, 20), RGB::from_u8(0, 0, 0), rltk::to_cp437('#'));
+            }
+        }
+
+        //Move to the next tile
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
 //Function to move the player entity
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>(); //Get write access to the ECS's Position component storage
     let mut players = ecs.write_storage::<Player>(); //Get write access to the ECS's Player component storage
+    let map = ecs.fetch::<Vec<TileType>>(); //Fetch the tile map from the ECS
 
     for (_player, pos) in (&mut players, &mut positions).join() { //All entities with both a Player component and a Position component
-        pos.x = min(79 , max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79 , max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -79,26 +146,9 @@ fn user_input(gs: &mut State, ctx: &mut Rltk) {
     }
 }
 
-//LeftMover system
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, //System needs read access to the ECS's LeftMover component storage
-                    WriteStorage<'a, Position>); //System needs write access to the ECS's Position component storage
-
-    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() { //All entities with both a LeftMover component and a Position component
-            pos.x -= 1;
-            if pos.x < 0 { pos.x = 79; }
-        }
-    }
-}
-
 //Function to run systems
 impl State {
     fn run_systems(&mut self) {
-        let mut lw = LeftWalker{};
-        lw.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -112,7 +162,8 @@ fn main() -> rltk::BError {
         .with_tile_dimensions(16, 16)
         .build()?;
     context.with_post_scanlines(true);
-    context.screen_burn_color(RGB::from_u8(170, 105, 50));
+    //context.screen_burn_color(RGB::from_u8(170, 105, 50));
+    context.screen_burn_color(RGB::from_u8(0, 0, 0));
 
     //Set the game state with a new ECS (World)
     let mut gs = State { 
@@ -121,8 +172,10 @@ fn main() -> rltk::BError {
     //Tell the ECS (World) about the components so it can store them
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+
+    //Add a new randomly generated map to the ECS as a resource
+    gs.ecs.insert(new_map());
 
     //Create a player entity with Position and Renderable components and a Player tag component
     gs.ecs
@@ -130,26 +183,12 @@ fn main() -> rltk::BError {
         .with(Position { x: 40, y: 25 })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
-            fg: RGB::named(rltk::YELLOW),
+            fg: RGB::named(rltk::WHITE),
             bg: RGB::named(rltk::BLACK),
         })
         .with(Player{})
         .build();
-    
-    //Add some more entities
-    for i in 0..10 {
-        gs.ecs
-        .create_entity()
-        .with(Position { x: i * 7, y: 20 })
-        .with(Renderable {
-            glyph: rltk::to_cp437('☺'),
-            fg: RGB::named(rltk::RED),
-            bg: RGB::named(rltk::BLACK),
-        })
-        .with(LeftMover{})
-        .build();
-    }
-    
+
     //Start the RLTK main loop
     rltk::main_loop(context, gs)
 }
